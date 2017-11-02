f9c4c1c9cf67c12f183a1ede4ed4fad7f947ffc1
advcode <ovi@advancode.com>
2017-11-02 08:16:52 -0400

    '

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	h "echo-hero/handler"
+	"time"
 
 	"github.com/labstack/echo"
+	"go.uber.org/zap"
 )
 
 func main() {
+
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	logger.Info("failed to fetch URL",
+		// Structured context as strongly typed Field values.
+		//zap.String("url", url),
+		//zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+
 	e := echo.New()
 	e.Static("/static", "static")
 	e.GET("/", h.Hello)
